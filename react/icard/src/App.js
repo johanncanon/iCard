f4733fe8262d0e108df706fc2c1f10c76c3b13e1
johanncanon <johanncanon@gmail.com>
2024-11-23 18:21:31 -0500

    new react project created

---
+++ react/icard/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from 'semantic-ui-react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hola Mundo!</h1>
+      <Button primary>Clic Me</Button>
+    </div>
+  )
+}
