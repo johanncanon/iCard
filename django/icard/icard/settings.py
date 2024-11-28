a69239f082cf55429a2f5a983fc73902403d069e
johanncanon <johanncanon@gmail.com>
2024-11-28 14:50:39 -0500

    new user api configured with django

--- django/icard/icard/settings.py
+++ django/icard/icard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#USERS
+AUTH_USER_MODEL = 'users.User'
